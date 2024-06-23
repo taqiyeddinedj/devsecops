61e8acc73b4f6c6f90b0d995432308d0e3ceeee8
taqiyeddinedj <djouani.taqiyeddine@gmail.com>
2024-06-23 11:44:06 +0000

    n

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func main() {
 		start := time.Now()
 
 		time.Sleep(600 * time.Millisecond) //Sleep simulate work
-		fmt.Fprint(w, "Welcome to my application!, did you see the change!!!!!!!!!!, i am adding a new line here, adding a new aline")
+		fmt.Fprint(w, "Welcome to my application!, did you see the change!!!!!!!!!!, i am adding a new line here, adding a new aline, this is a new line that has been added")
 	
 		//measure the duration and log to prometheus
 		httpDuration := time.Since(start)
